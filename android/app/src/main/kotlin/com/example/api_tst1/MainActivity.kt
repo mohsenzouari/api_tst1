4502f27e368f348004c9c73439a11f06a8f35f05
Mohsen Zouari <mohsennzouari@gmail.com>
2022-10-31 12:32:21 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/api_tst1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.api_tst1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized()
+    ..scheduleAttachRootWidget(const MyApp())
+    ..scheduleWarmUpFrame();
+}
+
+
