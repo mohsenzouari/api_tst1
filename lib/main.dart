d8766f8c03cebb903ff59bb51d1958aaaa791a14
Mohsen Zouari <mohsennzouari@gmail.com>
2022-11-01 22:01:07 +0100

    api with FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized()
-    ..scheduleAttachRootWidget(const MyApp())
-    ..scheduleWarmUpFrame();
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-qd
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ApiTst(),
+      // debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class ApiTst extends StatefulWidget {
+  const ApiTst({super.key});
+
+  @override
+  State<ApiTst> createState() => _ApiTstState();
+}
+
+class _ApiTstState extends State<ApiTst> {
+  List posts = [];
+
+  Future getPost() async {
+    var url = "https://jsonplaceholder.typicode.com/posts";
+    var response = await http.get(Uri.parse(url));
+    var responsebody = jsonDecode(response.body);
+    return responsebody;
+    // setState(() {
+    //   posts.addAll(responsebody);
+    // });
+    // print(responsebody[1]);
+  }
+
+  // @override
+  // void initState() {
+  //   getPost();
+  //   super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Dialog'),
+        ), // AppBar
+        body: Padding(
+            padding: const EdgeInsets.all(27.0),
+            child: FutureBuilder(
+              future: getPost(),
+              builder: ((context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          color: Colors.white,
+                          margin: EdgeInsets.only(top: 10, bottom: 10),
+                          child: Text("${snapshot.data[index]['id']}"),
+                        );
+                      });
+                }
+                return CircularProgressIndicator();
+              }),
+            )));
+  }
+}
---
+++ lib/tst.dart
@@ -0,0 +1,11 @@
+// body: posts == null II posts. isEmpty
+//       Center (child: CircularProgressIndicator
+//                                                "
+//      : ListView. builder (
+//          itemCount: posts. length,
+//          itemBuilder: (context, i) {
+//            return Container
+//              color: Colors.white,
+//              margin: EdgeInsets.only(top: 10 , bottom: 10), 3
+//              child: Text ("${ {posts [i]['title'])")); // Container
+//          }); // ListView. builder // Scaffold
